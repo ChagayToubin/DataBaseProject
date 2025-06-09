fdc6b91ad2e33762434976e509ce7cf077e300ec
chagay <chagaytoubin@gmail.com>
2025-06-09 12:46:57 +0300

    finsih the architecture

---
+++ Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    static class Menu
+    {
+        public static void ShowMainMune()
+        {
+
+            switch (Console.ReadLine())//להסויף ןלידציה
+            {
+                case "1"://
+
+
+                    Console.WriteLine();
+
+                    break;
+                case "2":
+                    Console.WriteLine();
+                    break;
+                case "3":
+                    Console.WriteLine();
+                    break;
+                case "4":
+                    Console.WriteLine();
+                    break;
+                    
+                default:
+                    Console.WriteLine("Please enter a valid value or 'q' to exit the system.");
+                    ShowMainMune();
+                    break;
+
+            }
+        }
+
+    }
+}
--- obj/Debug/net8.0/DataBase.AssemblyInfo.cs
+++ obj/Debug/net8.0/DataBase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataBase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcab21a8fdc6a0f5760b5ca6abb2556680ab7b43")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataBase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataBase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
